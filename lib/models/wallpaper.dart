ec3d3570ec65c37055ccc2e9d4228ad6f8ccebc5
soufiyantiger <mrsoufiyantiger@gmail.com>
2022-02-02 11:55:24 +0100

    Generate Adapter

--- lib/models/wallpaper.dart
+++ lib/models/wallpaper.dart
@@ -1,14 +1,20 @@
+import 'package:hive/hive.dart';
 import 'urls.dart';
 
+part 'wallpaper.g.dart';
+
+@HiveType(typeId: 0)
 class Wallpaper {
   Wallpaper({
     required this.description,
     required this.altDescription,
     required this.urls,
   });
-
+  @HiveField(0)
   String description;
+  @HiveField(1)
   String altDescription;
+  @HiveField(2)
   Urls urls;
 
   factory Wallpaper.fromJson(Map<String, dynamic> json) => Wallpaper(
